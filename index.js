6fb03756933dcea8f7e600689a1042f1dc430da2
Jung MinHyuk <chmh0805@naver.com>
2023-05-17 14:40:14 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jQuery Bug Test</title>
+</head>
+
+<body>
+    <div>
+        <h1>jQuery 1.12.4</h1>
+        <iframe src="./jquery_1_12_4.html"></iframe>
+    </div>
+    <div>
+        <h1>jQuery 3.6.4</h1>
+        <iframe src="./jquery_3_6_4.html"></iframe>
+    </div>
+</body>
+
+</html>
